2d478618963d74283b4acc211a26cd98d6d8aa52
saranj <saranj.bule@gmail.com>
2023-03-28 15:58:35 +0530

    added contact app

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import messages
+from .models import Contact
+from django.core.mail import send_mail
+def contact(request):
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
 
-# Create your views here.
+        # checking if user has made inquiry already
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+
+            if has_contacted:
+                messages.error(request, "User already made a request")
+                return redirect('/listings/'+listing_id)
+            
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        # send mail
+        send_mail(
+              "Property Listing Inquiry",
+              "There has been an inquiry for"+listing+'. Sign in for more info',
+               'saranj.bule@gmail.com',
+               ['saranj@trendlyne.com'],
+               fail_silently=False
+        )
+
+        messages.success(request, 'Request has been submitted')
+
+        return redirect('/listings/'+listing_id)
+
+
--- real_estate/settings.py
+++ real_estate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contacts.apps.ContactsConfig',
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
@@ -144,3 +145,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'saranj.bule@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS=True
